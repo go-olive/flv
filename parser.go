fac33510d0188149d6aee98f4727c27535a20a2e
luxcgo <luxcgo@gmail.com>
2022-04-17 16:21:03 +0800

    parse: parse live stream

---
+++ parser.go
@@ -0,0 +1,104 @@
+package flv
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+)
+
+type Parser struct {
+	closeOnce sync.Once
+	stop      chan struct{}
+
+	avcHeader []byte
+}
+
+func NewParser() *Parser {
+	return &Parser{
+		stop: make(chan struct{}),
+	}
+}
+
+func (p *Parser) Stop() {
+	p.closeOnce.Do(func() {
+		close(p.stop)
+	})
+}
+
+func (p *Parser) Type() string {
+	return "flv"
+}
+
+func (p *Parser) Parse(streamUrl string, out string) (err error) {
+	req, err := http.NewRequest("GET", streamUrl, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("User-Agent", "Chrome/59.0.3071.115")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	d, err := NewDemuxer(resp.Body)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	o, err := os.Create(out)
+	if err != nil {
+		return err
+	}
+
+	m, err := NewMuxer(o)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	flvHeader := GetHeaderCompo()
+	d.ReadHeader(flvHeader)
+	m.WriteHeader(flvHeader)
+	flvHeader.Put()
+
+	for {
+		select {
+		case <-p.stop:
+			return nil
+		default:
+			flvBody := new(TagCompo)
+
+			err := d.ReadTag(flvBody)
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				} else {
+					return err
+				}
+			}
+
+			if flvBody.ContainAVCSeqHeader() {
+				if p.avcHeader == nil {
+					p.avcHeader = make([]byte, len(flvBody.TagBodyRaw))
+					copy(p.avcHeader, flvBody.TagBodyRaw)
+				} else {
+					if bytes.Compare(p.avcHeader, flvBody.TagBodyRaw) == 0 {
+						flvBody.Free()
+						continue
+					} else {
+						flvBody.Free()
+						return fmt.Errorf("video(name = %s) AVC sequence header changed", out)
+					}
+				}
+			}
+			m.WriteTag(flvBody)
+			flvBody.Free()
+		}
+	}
+
+}
--- tag.go
+++ tag.go
@@ -2,6 +2,17 @@ package flv
 
 import (
 	"encoding/binary"
+	"math"
+)
+
+var (
+	sig = [3]uint8{0x46, 0x4c, 0x56}
+	ver = uint8(0x01)
+	do  = uint32(9)
+
+	frameRate           = 15.0
+	videoFrameInterval  = math.Ceil(1000 / frameRate)
+	soundSampleInterval = math.Ceil(1000 / 44)
 )
 
 type HeaderCompo struct {
@@ -11,11 +22,29 @@ type HeaderCompo struct {
 }
 
 type Header struct {
-	Signature       [3]uint8
-	Version         uint8
+	Signature [3]uint8
+	Version   uint8
+	// the below four vars combine as a var TypeFlags
+	// TypeFlagsReserved1 [5]uint1
+	// TypeFlagsAudio     uint1
+	// TypeFlagsReserved2 uint1
+	// TypeFlagsVideo     uint1
 	TypeFlags       uint8
 	DataOffset      uint32
 	PreviousTagSize uint32
+}
+
+func (this *Header) Valid() bool {
+	return this.Signature == sig &&
+		this.Version == ver &&
+		this.DataOffset == do
+}
+
+func (this *Header) HasAudio() bool {
+	return this.TypeFlags&uint8(0b00000100) != 0
+}
+func (this *Header) HasVedio() bool {
+	return this.TypeFlags&uint8(0b00000001) != 0
 }
 
 type TagCompo struct {
@@ -33,11 +62,14 @@ func (this *TagCompo) Free() {
 }
 
 type TagHeader struct {
-	TagType           byte
-	DataSize          []byte
+	TagType byte
+	// 3 bytes
+	DataSize []byte
+	// 4 bytes
 	Timestamp         []byte
 	TimestampExtended byte
-	StreamID          []byte
+	// 3 bytes
+	StreamID []byte
 }
 
 func (this *TagHeader) GetDataSize() uint32 {
@@ -53,4 +85,21 @@ func (this *TagHeader) GetTimestamp() uint32 {
 	realTimestamp := binary.BigEndian.Uint32(append([]uint8{this.TimestampExtended}, []uint8(this.Timestamp[:])...))
 	// log.Printf("start time: %s", time.Unix(int64(fixedTimstamp/1000), 0))
 	return realTimestamp
+}
+
+func (this *TagCompo) ContainAVCSeqHeader() bool {
+	if this.TagHeader.TagType != 9 {
+		return false
+	}
+	if len(this.TagBodyRaw) < 2 {
+		return false
+	}
+	if (this.TagBodyRaw[0] & 15) != 7 {
+		return false
+	}
+	if this.TagBodyRaw[1] == 0 {
+		return true
+
+	}
+	return false
 }
